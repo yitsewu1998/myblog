5395dbd49cf468e9fd55fa87d7a718af16dfcc97
巫逸哲 <yitsewu@yitsewu.local>
2019-03-10 14:30:06 +0800

    產生後台

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.utils import timezone
+
+from .models import Post
 
 # Create your views here.
 
@@ -7,4 +10,14 @@ def post_list(request):
 
 
 def post_create(request):
-	return render(request, 'blog/post_create.html', {})
+    if request.method == "POST":
+        new_title = request.POST.get('title')
+        new_text = request.POST.get('text')
+        new_post = Post(title=new_title, text=new_text)
+        
+        new_post.save()
+
+        new_post.publish()        
+        return redirect('post_list')
+
+    return render(request, 'blog/post_create.html', {})
